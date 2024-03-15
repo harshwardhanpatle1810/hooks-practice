77dc9b11024a927c460b0520e7e87ed1d5067315
harshwardhanpatle1810 <harshwardhanpatle1810@gmail.com>
2024-03-15 15:21:17 -0700

    add code of hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./App.css";
 
 function App() {
+
+  const[count,setCount]=useState(0);
+
+  function inc(){
+    setCount(count+1);
+  }
+
+  function dec(){
+    setCount(count-1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="counter-text" >Count: <span>{count}</span></h1>
+      <div className="btn-container">
+        <button className="btn" onClick={inc}>Increament</button>
+        <button className="btn" onClick={dec}>Decreament</button>
+      </div>
     </div>
   );
 }
