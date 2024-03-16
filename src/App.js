9daab058002a1ca8980a91b5bd71a936f3368e0c
harshwardhanpatle1810 <harshwardhanpatle1810@gmail.com>
2024-03-16 14:13:07 -0700

    add updating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React,{useState} from "react";
+import React, { useState,useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const[warning,setWarning]=useState('');
 
-  const[count,setCount]=useState(0);
+  useEffect(()=>{},[count])
 
-  function inc(){
-    setCount(count+1);
-  }
+  useEffect(()=>{
+    if(count < 0){
+      setWarning('Count cannot be than 0');
+    }
+    else if(count >10){
+      setWarning('Count cannot be more than 10');
+    }
+    else{
+      setWarning('');
+    }
+  },[count])
+ 
 
-  function dec(){
-    setCount(count-1);
-  }
   return (
     <div>
-      <h1 className="counter-text" >Count: <span>{count}</span></h1>
+      <h1 className="counter-text">
+        Count: <span>{count}</span>
+      </h1>
+      <p className="warning-text">{warning}</p>
       <div className="btn-container">
-        <button className="btn" onClick={inc}>Increament</button>
-        <button className="btn" onClick={dec}>Decreament</button>
+        <button
+          className="btn"
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Increament
+        </button>
+        <button
+          className="btn"
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          Decreament
+        </button>
       </div>
     </div>
   );
